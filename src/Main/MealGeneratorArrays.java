8c3c76786866c2b396a75692cfe3444a7251fd72
BuildTools <unconfigured@null.spigotmc.org>
2021-04-18 11:40:50 -0600

    added method to generate array of meals for a given number of days.

--- src/Main/MealGeneratorArrays.java
+++ src/Main/MealGeneratorArrays.java
@@ -1,17 +1,92 @@
 package Main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Random;
+
 public class MealGeneratorArrays {
-    public String[] genArray(int days){
-        //todo: create method to generate a array of size days with meals for each day.(maybe try to avoid repetition?)
-        return null;
+    public Object[][] genArray(int days){
+        FileIO fileIO = new FileIO();
+        Random random = new Random();
+
+        Object[] mealArray = fileIO.readAllObj();
+        int rowSize = 7;
+        ArrayList<Object> mealArrayList = new ArrayList<>(Arrays.asList(mealArray));
+        Object[][] mealArrayFinal = new Object[(int)(Math.ceil((days / (double)rowSize)))][rowSize];
+        int j;
+        for (j = 0; j < days / rowSize ;j++){
+
+            for (int i = 0; i < rowSize; i++){
+                int randIndex = random.nextInt(mealArrayList.size());
+                mealArrayFinal[j][i] = mealArrayList.remove(randIndex);
+                if (mealArrayList.isEmpty()){
+                    mealArrayList = new ArrayList<>(Arrays.asList(mealArray));
+                }
+            }
+        }
+        if (!(days % rowSize == 0)){
+            Arrays.fill(mealArrayFinal[mealArrayFinal.length - 1], (Object)0);
+            for (int i = 0; i < days - (j * rowSize); i++ ){
+                int randIndex = random.nextInt(mealArrayList.size());
+                mealArrayFinal[j][i] = mealArrayList.remove(randIndex);
+                if (mealArrayList.size() == 0){
+                    mealArrayList = new ArrayList<>(Arrays.asList(mealArray));
+                }
+            }
+        }
+
+        return mealArrayFinal;
     }
-    public String[][] genArray(int month, int day){
+    public Object[][] genArray(int month, int day){
         //todo: create method to generate a array with meals for each day of the supplied month.(maybe try to avoid repetition?)
         return null;
     }
-    public String[][] genArray(String month, int day){
-        //todo: convert string month to int and pass it to genArray as a int.
-        // (we could swap this if we want months to be handled as strings)
-        return null;
+    public Object[][] genArray(String month, int day){
+        int intMonth;
+
+        month = month.toLowerCase(Locale.ROOT);
+
+        switch (month){
+            case "january" :
+                intMonth = 1;
+                break;
+            case "february":
+                intMonth = 2;
+                break;
+            case "march":
+                intMonth = 3;
+                break;
+            case "april":
+                intMonth = 4;
+                break;
+            case "may":
+                intMonth = 5;
+                break;
+            case "june":
+                intMonth = 6;
+                break;
+            case "july":
+                intMonth = 7;
+                break;
+            case "august":
+                intMonth = 8;
+                break;
+            case "september":
+                intMonth = 9;
+                break;
+            case "october":
+                intMonth = 10;
+                break;
+            case "november":
+                intMonth = 11;
+                break;
+            case "december":
+                intMonth = 12;
+                break;
+            default:
+                intMonth = 0;
+        }
+        return genArray(intMonth, day);
     }
 }
